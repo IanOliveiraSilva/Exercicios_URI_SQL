a5d418b4658ee7ffc3e79861d6189ddd94143f8a
Vesper03 <xxndtry33@gmail.com>
2021-12-20 23:23:23 -0300

    Exercicio SQL 2613

--- 2612.sql
+++
@@ -1,5 +0,0 @@
-Select a.id, a.name 
-FROM movies AS a
-INNER JOIN prices AS b
-ON a.id_prices = b.id
-WHERE b.value < 2.00
--- 2613.sql
+++ 2613.sql
@@ -1,5 +1,5 @@
 Select a.id, a.name 
-FROM movies AS a
-INNER JOIN prices AS b
+FROM movies as a
+INNER JOIN prices as b
 ON a.id_prices = b.id
 WHERE b.value < 2.00
